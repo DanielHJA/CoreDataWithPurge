669d52c3733bcb63248a2c0e493a6dc8e3a0abf5
Daniel <daniel.hjartstrom@gmail.com>
2019-08-14 08:43:44 +0200

    Initial Commit

---
+++ CoreDataCRUD/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataCRUD
+//
+//  Created by Daniel Hjärtström on 2019-08-14.
+//  Copyright © 2019 Sog. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
